a9027ffbd908c078412ad6cc4d79c2488de88d94
Ashley Mannix <ashleymannix@live.com.au>
2019-10-24 08:40:14 +1000

    try patching corert's objwriter to llilc build

--- src/build.sh
+++ src/build.sh
@@ -2,74 +2,92 @@
 
 set -o errexit -o nounset
 
-# coreclr
+# src: coreclr
 mkdir -p /dotnet/src/coreclr && cd /dotnet/src/coreclr
 git clone https://github.com/dotnet/coreclr.git .
 git checkout $CORECLR_REF
-cd /dotnet/src/coreclr && ./build.sh -x64 -checked
-rm -r .git
 
-# llilc
+# src: corefx
+mkdir -p /dotnet/src/corefx && cd /dotnet/src/corefx
+git clone https://github.com/dotnet/corefx.git .
+git checkout $COREFX_REF
+
+# src: corert
+mkdir -p /dotnet/src/corert && cd /dotnet/src/corert
+git clone https://github.com/dotnet/corert.git .
+git checkout $CORERT_REF
+
+# src: core-sdk
+mkdir -p /dotnet/src/core-sdk && cd /dotnet/src/core-sdk
+git clone https://github.com/dotnet/core-sdk.git .
+git checkout $CORESDK_REF
+
+# src: core-setup
+mkdir -p /dotnet/src/core-setup && cd /dotnet/src/core-setup
+git clone https://github.com/dotnet/core-setup.git .
+git checkout $CORESETUP_REF
+
+# src: diagnostics
+mkdir -p /dotnet/src/diagnostics && cd /dotnet/src/diagnostics
+git clone https://github.com/dotnet/diagnostics.git .
+git checkout $DIAGNOSTICS_REF
+
+# src: llilc
 mkdir -p /dotnet/src/llvm && cd /dotnet/src/llvm
 git clone -b MS https://github.com/microsoft/llvm .
 git checkout $MSLLVM_REF
-rm -r .git
 mkdir build && mkdir -p tools/llilc && cd tools/llilc
 git clone https://github.com/dotnet/llilc .
 git checkout $LLILC_REF
-rm -r .git
+
+# build: coreclr
+cd /dotnet/src/coreclr && ./build.sh -x64 -checked
+
+# build: llilc
 cd /dotnet/src/llvm/build
+rm -r tools/llilc/lib/ObjWriter
+cp /dotnet/src/corert/Native/ObjWriter tools/llilc/lib
 cmake \
     -DCMAKE_C_COMPILER=clang-3.9 \
     -DCMAKE_CXX_COMPILER=clang++-3.9 \
     -DWITH_CORECLR=/dotnet/src/coreclr/bin/Product/Linux.x64.Checked \
-    -DLLVM_OPTIMIZED_TABLEGEN=ON ..
+    -DLLVM_OPTIMIZED_TABLEGEN=ON \
+    -DLLVM_INCLUDE_TESTS=0 \
+    -DLLVM_TARGETS_TO_BUILD="X86" \
+    ..
 make
 
-# corefx
-mkdir -p /dotnet/src/corefx && cd /dotnet/src/corefx
-git clone https://github.com/dotnet/corefx.git .
-git checkout $COREFX_REF
+# build: corefx
 cd /dotnet/src/corefx && ./build.sh --arch x64 --warnAsError false
-rm -r .git
 
-# corert
-mkdir -p /dotnet/src/corert && cd /dotnet/src/corert
-git clone https://github.com/dotnet/corert.git .
-git checkout $CORERT_REF
+# build: corert
 cd /dotnet/src/corert
 ./build.sh x64 skiptests
 ./buildscripts/build-packages.sh x64
-rm -r .git
 
-# diagnostics
-mkdir -p /dotnet/src/diagnostics && cd /dotnet/src/diagnostics
-git clone https://github.com/dotnet/diagnostics.git .
-git checkout $DIAGNOSTICS_REF
+# build: diagnostics
 cd /dotnet/src/diagnostics && ./build.sh --architecture x64
-rm -r .git
 
-# core-sdk
-mkdir -p /dotnet/src/core-sdk && cd /dotnet/src/core-sdk
-git clone https://github.com/dotnet/core-sdk.git .
-git checkout $CORESDK_REF
+# build: core-sdk
 cd /dotnet/src/core-sdk && ./build.sh --architecture x64
-rm -r .git
 
-# core-setup
-mkdir -p /dotnet/src/core-setup && cd /dotnet/src/core-setup
-git clone https://github.com/dotnet/core-setup.git .
-git checkout $CORESETUP_REF
+# build: core-setup
 cd /dotnet/src/core-setup && ./build.sh \
     --restore --build --pack \
     /p:CoreCLROverridePath=/dotnet/src/coreclr/bin/Product/Linux.x64.Checked \
     /p:CoreFXOverridePath=/dotnet/src/corefx/artifacts/packages/Debug
-rm -r .git
 
-# distribution
 mkdir -p /dotnet/dist/bin && mkdir -p /dotnet/dist/packages
+
+# dist: sdk
 cp -r /dotnet/src/core-sdk/artifacts/bin/redist/Debug/dotnet/* /dotnet/dist/bin
+
+# dist: runtime
 cp -r /dotnet/src/core-setup/artifacts/obj/linux-x64.Debug/sharedFrameworkPublish/* /dotnet/dist/bin
 cp -r /dotnet/src/core-setup/artifacts/packages/Debug/Shipping/* /dotnet/dist/packages
+
+# dist: corert
 cp -r /dotnet/src/corert/bin/packages/Debug/* /dotnet/dist/packages
+
+# cleanup
 rm -r /dotnet/src
