09cba7f2cfa718dfda040169d19fffb16084cef7
Ashley Mannix <ashleymannix@live.com.au>
2019-11-08 10:47:14 +1000

    are we hanging?

--- src/build/build.sh
+++ src/build/build.sh
@@ -14,14 +14,8 @@ $SCRIPT_ROOT/platform/corert.sh
 $SCRIPT_ROOT/sdk/diagnostics.sh
 $SCRIPT_ROOT/sdk/roslyn.sh
 
-# TODO: Do we still need to wrap these ILC invocations like this?
-echo "Compiling csc with ilc..."
-0<&- script -qefc "$SCRIPT_ROOT/sdk/csc.sh" /dev/null | cat
-
-echo "Compiling crossgen with ilc..."
-0<&- script -qefc "$SCRIPT_ROOT/sdk/crossgen.sh" /dev/null | cat
-
-echo "Compiling r2rdump with ilc..."
-0<&- script -qefc "$SCRIPT_ROOT/sdk/r2rdump.sh" /dev/null | cat
+$SCRIPT_ROOT/sdk/csc.sh
+$SCRIPT_ROOT/sdk/crossgen.sh
+$SCRIPT_ROOT/sdk/r2rdump.sh
 
 $SCRIPT_ROOT/cleanup.sh
