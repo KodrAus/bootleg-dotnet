23df2bce6616d9971f0f983770d5fad430931119
Ashley Mannix <ashleymannix@live.com.au>
2019-11-13 19:26:11 +1000

    tweak nano styles for C#

--- src/build/etc/nano.sh
+++ src/build/etc/nano.sh
@@ -2,7 +2,4 @@
 
 set -o errexit -o nounset
 
-mkdir -p /dotnet/dist/etc/nanorc
-cp /dotnet/src/etc/nanorc/csharp.nanorc /dotnet/dist/etc/nanorc
-
 echo "include /dotnet/dist/etc/nanorc/csharp.nanorc" >> /etc/nanorc
